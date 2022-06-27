ea76c185844e7ae67beac99a550c81f10dfe5a60
Tyson Unce <Tyson@MacBook-Pro.attlocal.net>
2022-06-27 07:38:26 -0500

    lets see what happens

---
+++ client/script.js
@@ -0,0 +1,148 @@
+//WHENEVER THE PAGE LOADS IT WILL RUN THIS CALLBACK FUNCTION
+document.addEventListener('DOMContentLoaded', function () {
+     //MAKING OUR API CALL
+    fetch('http://localhost:5000/getAll')
+      //FETCH RETURNS A PROMISE, SO WE USE .THEN FOR AN ACTION IF THE CALL IS SUCCESSFULL,
+    .then(response => response.json())
+    .then(data => loadHTMLTable(data['data']));
+    
+});
+
+document.querySelector('table tbody').addEventListener('click', function(event) {
+      //THE EVENT TARGET CLASSNAME IS THE DATA THAT SPECIFCES WHAT WAS EFFECTED BY YOUR ACTION, SO BASICALLY THIS IS SAYING IF WE CLICKED THE DELETE BUTTON
+    if (event.target.className === "delete-row-btn") {
+        deleteRowById(event.target.dataset.id);
+    }
+    if (event.target.className === "edit-row-btn") {
+        handleEditRow(event.target.dataset.id);
+    }
+});
+
+const updateBtn = document.querySelector('#update-row-btn');
+
+
+//FETCHING DATA FROM THE DELETE CRUD OP
+function deleteRowById(id) {
+    fetch('http://localhost:5000/delete/' + id, {
+        method: 'DELETE'
+    })
+    .then(response => response.json())
+      //WHAT WE'RE SAYING HERE IS IF OUR DELETE WAS SUCCSESSFUL, WE'RE RELOADING THE PAGE TO SHOW THE CHANGES
+    .then(data => {
+        if (data.success) {
+            location.reload();
+        }
+    });
+}
+
+function handleEditRow(id) {
+    //IF EDIT BUTTON IS CLICKED THEN THE HIDDEN SECTION WILL NO LONGER BE HIDDEN, THUS HIDDEN === FALSE
+    const updateSection = document.querySelector('#update-row');
+    updateSection.hidden = false;
+    document.querySelector('#update-name-input').dataset.id = id;
+}
+
+updateBtn.onclick = function() {
+    const updateNameInput = document.querySelector('#update-name-input');
+
+
+    console.log(updateNameInput);
+
+    fetch('http://localhost:5000/update', {
+        method: 'PATCH',
+        headers: {
+            'Content-type' : 'application/json'
+        },
+        body: JSON.stringify({
+            id: updateNameInput.dataset.id,
+            name: updateNameInput.value
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            location.reload();
+        }
+    })
+}
+
+
+const addBtn = document.querySelector('#add-name-btn');
+
+addBtn.onclick = function () {
+      //MAKING IT TO WHERE WHENEVER A VALUE IS SUBMITTED THE VALUE IS RESET, IN ORDER TO ADD MORE NAMES EVENTUALLY
+    const nameInput = document.querySelector('#name-input');
+    const name = nameInput.value;
+    nameInput.value = "";
+
+    //SENDING THIS INFORMATION TO THE BACKEND
+    fetch('http://localhost:5000/insert', {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        //SINCE WE'RE SENDING DATA TO BACKEND WE WANT TO SEND AS POST, STRINGIFY TURNS OBJECT TO A JSON STRING
+        method: 'POST',
+        body: JSON.stringify({ name : name})
+    })
+    .then(response => response.json())
+    .then(data => insertRowIntoTable(data['data']));
+}
+
+function insertRowIntoTable(data) {
+    console.log(data);
+    const table = document.querySelector('table tbody');
+    //CHECKING TO SEE IF OUR NO-DATA CLASS IS PRESENT
+    const isTableData = table.querySelector('.no-data');
+
+    let tableHtml = "<tr>";
+
+    for (var key in data) {
+        //since this returns an object we have to see if the data has it's own property because if not it will return an error, and so with that key we will be pulling the data from that object
+        if (data.hasOwnProperty(key)) {
+            if (key === 'dateAdded') {
+                data[key] = new Date(data[key]).toLocaleString();
+            }
+            tableHtml += `<td>${data[key]}</td>`;
+        }
+    }
+
+     //WE USE DOT NOTATION HERE SINCE WE ARE NOT IN A LOOP WHERE THE OBJECT IS SPECIFIED
+    tableHtml += `<td><button class="delete-row-btn" data-id=${data.id}>Delete</td>`;
+    tableHtml += `<td><button class="edit-row-btn" data-id=${data.id}>Edit</td>`;
+
+    tableHtml += "</tr>";
+
+    //THE LOOP IS THE SAME AS SAYING IF THERE IS NO TABLE DATA, THEN THE HTML WILL OVERRIDE IT, SO OUR TABLE DATA IS INSTANTLY UPDATED
+    if (isTableData) {
+        table.innerHTML = tableHtml;
+    } else {
+        const newRow = table.insertRow();
+        newRow.innerHTML = tableHtml;
+    }
+}
+
+function loadHTMLTable(data) {
+    //SELECTING THE TABLE TBODY WITH THE QUERY SELECTOR AND ASSIGNING IT A BODY
+    const table = document.querySelector('table tbody');
+
+    //IF NO DATA IS ENTERED THE TABLE WILL DISPLAY 'NO DATA'
+    if (data.length === 0) {
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+        return;
+    }
+
+     //LOOP THAT ADDS DATA TO THE TABLE FROM THE DATA IN MYSQL, WHEN DATA IS ENTERED IT WILL DISPLAY EDIT AND DELETE BUTTONS ASWELL
+    let tableHtml = "";
+
+    data.forEach(function ({id, name, date_added}) {
+        tableHtml += "<tr>";
+        tableHtml += `<td>${id}</td>`;
+        tableHtml += `<td>${name}</td>`;
+        tableHtml += `<td>${new Date(date_added).toLocaleString()}</td>`;
+        tableHtml += `<td><button class="delete-row-btn" data-id=${id}>Delete</td>`;
+        tableHtml += `<td><button class="edit-row-btn" data-id=${id}>Edit</td>`;
+        tableHtml += "</tr>";
+    });
+
+    table.innerHTML = tableHtml;
+}
---
+++ server/dbService.js
@@ -0,0 +1,136 @@
+//ACCESSING MYSQL
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+let instance = null;
+dotenv.config();
+
+//CREATING A MYSQL CONNECTION
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+});
+
+// SPECIFYING IF CONNECTION TO OUR DATABASE IS SUCCESSFUL OR UNSUCCESSFUL
+connection.connect((err) => {
+    if (err) {
+        console.log(err.message);
+    }
+});
+
+
+//STATIC IS SO THERE'S ONE INSTANCE OF THIS FUNCTION THAT IS SHARED BY ALL OBJECTS OF CLASS
+class DbService {
+    static getDbServiceInstance() {
+        // IF INSTANCE IS NOT NULL RETURN OBJECT CREATED, IF IS NULL RETURN A NEW INSTANCE OF DBSERVICE
+        return instance ? instance : new DbService();
+    }
+
+    async getAllData() {
+        //IF QUERY IS SUCCESSFUL WE RESOLVE IT, IF NOT WE REJECT, THE REJECT WILL GO INTO CATCH BLOCK AND WE'LL HANDLE
+        try {
+            const response = await new Promise((resolve, reject) => {
+
+                 //WRITING OUR SQL QUERY
+                //EXAMPLE OF CONNECTION QUERY WITH CONDITION: const query = "SELECT * FROM names WHERE id = ?"
+                const query = "SELECT * FROM names;";
+
+                 //EXAMPLE CONT.: connection.query(query, [id])
+                //IF QUERY IS UNSUCCESSFUL THE ERROR.MESSAGE WILL BE CAUGHT BY OUR CATCH BLOCK
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+            // console.log(response);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    async insertNewName(name) {
+        try {
+            const dateAdded = new Date();
+            const insertId = await new Promise((resolve, reject) => {
+
+                //WRITING OUR SQL QUERY
+                //EXAMPLE OF CONNECTION QUERY WITH CONDITION: const query = "SELECT * FROM names WHERE id = ?"
+                const query = "INSERT INTO names (name, date_added) VALUES (?,?);";
+
+                 //EXAMPLE CONT.: connection.query(query, [id])
+                //IF QUERY IS UNSUCCESSFUL THE ERROR.MESSAGE WILL BE CAUGHT BY OUR CATCH BLOCK
+                connection.query(query, [name, dateAdded] , (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.insertId);
+                })
+            });
+            //FOR OUR INSERT FUNCTION, WHEN ADDING A NEW NAME, WE NEED THE NAME DATE AND ID ADDED SO WE RETURN AN OBJECT WITH THOSE VALUES
+            return {
+                id : insertId,
+                name : name,
+                dateAdded : dateAdded
+            };
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async deleteRowById(id) {
+        try {
+            id = parseInt(id, 10); 
+            const response = await new Promise((resolve, reject) => {
+
+            //WRITING OUR SQL QUERY
+            //EXAMPLE OF CONNECTION QUERY WITH CONDITION: const query = "SELECT * FROM names WHERE id = ?"
+                const query = "DELETE FROM names WHERE id = ?";
+                
+                //EXAMPLE CONT.: connection.query(query, [id])
+                //IF QUERY IS UNSUCCESSFUL THE ERROR.MESSAGE WILL BE CAUGHT BY OUR CATCH BLOCK
+                connection.query(query, [id] , (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                })
+            });
+            
+            //IF RESPONSE IS EQUAL 1 RETURN TRUE, OTHERWISE RETURN FALSE, THIS SHOULD ALWAYS RETURN 1 BECAUSE IT SHOULD ALWAYS HAVE 1 AFFECTED ROW
+            return response === 1 ? true : false;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async updateNameById(id, name) {
+        try {
+            id = parseInt(id, 10); 
+            const response = await new Promise((resolve, reject) => {
+
+            //WRITING OUR SQL QUERY
+            //EXAMPLE OF CONNECTION QUERY WITH CONDITION: const query = "SELECT * FROM names WHERE id = ?"
+                const query = "UPDATE names SET name = ? WHERE id = ?";
+                
+                //EXAMPLE CONT.: connection.query(query, [id])
+                //IF QUERY IS UNSUCCESSFUL THE ERROR.MESSAGE WILL BE CAUGHT BY OUR CATCH BLOCK
+                connection.query(query, [name, id] , (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                })
+            });
+    
+            //IF RESPONSE IS EQUAL 1 RETURN TRUE, OTHERWISE RETURN FALSE, THIS SHOULD ALWAYS RETURN 1 BECAUSE IT SHOULD ALWAYS HAVE 1 AFFECTED ROW
+            return response === 1 ? true : false;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    
+}
+
+// EXPORTING MODULE(FILE) IN ORDER TO USE THE FUNCTIONS OF THIS MODULE IN OTHER MODULES
+module.exports = DbService;
